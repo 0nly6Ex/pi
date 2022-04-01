3d334108e111de9040f10c454e844f991daef158
0nly6Ex <76264172+0nly6Ex@users.noreply.github.com>
2022-04-01 14:36:32 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from threading import Thread
+try:
+    # import version included with old SymPy
+    from sympy.mpmath import mp
+except ImportError:
+    # import newer version
+    from mpmath import mp
+app = Flask(__name__)  
+
+@app.route('/')
+def my_form():
+    return render_template("pi.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    if text1.isnumeric():
+      if int(text1) < 250000: 
+        mp.dps = int(text1)
+        return ('<!DOCTYPE html><html lang="en"><head> <style>*{font-family: courier; color: black;}</style></head><body> <p>'+str(mp.pi)+'</p></body></html>')
+      else:
+        return("MAX NUMBER: 250,000")
+    else:
+      return("INVALID")
